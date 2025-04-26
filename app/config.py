97587325a24a806e3b210071736eb04fa7d53a72
Rahma12343 <111107309+Rahma12343@users.noreply.github.com>
2025-04-27 00:57:51 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,13 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
-cred = credentials.Certificate("firebase-adminsdk.json")
+# ✅ أخذ بيانات الاعتماد من متغير البيئة بدل ملف
+firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
+cred = credentials.Certificate(json.loads(firebase_credentials))
 
-# تأكد أنه ما تم تهيئة Firebase مسبقًا
+# مسجّلًا Firebase أنك لم تهيئه مسبقاً
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-# ⬅️ يمثل الموظف الأمني الحالي الذي سجل الدخول
+# ➡️ يمثل الموظف الأمني الحالي الذي سجل الدخول
 current_security_id = None
